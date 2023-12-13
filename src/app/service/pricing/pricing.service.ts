75ffec3f0c0f11e29fe3f2ea92ee0c843f7e2d72
dinhtv7 <nguyenduyquangvinh2906@gmail.com>
2023-12-14 04:19:34 +0700

    vinhndq - hot fix config module

--- src/app/service/pricing/pricing.service.ts
+++ src/app/service/pricing/pricing.service.ts
@@ -1,19 +1,37 @@
 import {Injectable} from '@angular/core';
 import {environment} from "../../../environments/environment";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {SettingService} from "../setting/setting.service";
+import {AuthService} from "../auth/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PricingService {
   private pricingApiUrl: string = environment.apiPublicv1 + 'pricings';
+  private pricingByTenantApiUrl: string = environment.apiTennatv1 + 'pricings';
+  protected header: HttpHeaders | any;
+  protected token: string | null;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private settingService: SettingService,
+    private authSerivce: AuthService,
   ) {
+    this.token = this.authSerivce.getToken();
+    this.header = {
+      headers: new HttpHeaders()
+        .set('Authorization', `${this.token}`)
+    }
   }
 
   get list() {
     return this.http.get(this.pricingApiUrl);
+  }
+
+  get() {
+    return this.http.post(`${this.pricingByTenantApiUrl}`, {
+      domain_name: this.settingService.tenant?.name
+    }, this.header);
   }
 }
